c474de83f1f7db92770adee34d2c6a00a818557f
wuzk <wuzk@flatincbr.com>
2023-02-16 10:31:04 +0800

    破解验证码

--- index.js
+++ index.js
@@ -4,93 +4,103 @@ const puppeteer = require('puppeteer');
 
 const dotenv = require("dotenv")
 dotenv.config()
-const {sleep, liveClick, analysisClick, getLivingData, getAnalysisLivingData, getLiveRoomData} = require('./js/common');
+const {calculateDistance, sleep, liveClick, analysisClick, getLivingData, getAnalysisLivingData, getLiveRoomData} = require('./js/common');
 
 // 最终需要的数据集合
 const dataMap = {
-        'getLiveRoomData': undefined,
-        'getLivingData': undefined,
-        'getAnalysisLivingData': undefined
-    }
+    'getLiveRoomData': undefined,
+    'getLivingData': undefined,
+    'getAnalysisLivingData': undefined
+  }
 
 ;(async () => {
-    try {
-        const page = await openBrowser() // 打开浏览器
-        await login(page) // 登录
-        await sleep(3000); // 等待3s进入首页 仅等待是不好确保的，需要做更细致的区分，此处简化
-        console.log('监听需要的响应')
-        await listen(page)
-        await sleep(1000)
-        console.log('触发事件如点击tab')
-        await emit(page)
-        await sleep(3000)
-        console.log('查看数据')
-        console.log(dataMap)
+  try {
+    const page = await openBrowser() // 打开浏览器
+    await login(page) // 登录
+    await sleep(3000); // 等待3s进入首页 仅等待是不好确保的，需要做更细致的区分，此处简化
+    console.log('监听需要的响应')
+    await listen(page)
+    await sleep(1000)
+    console.log('触发事件如点击tab')
+    await emit(page)
+    await sleep(3000)
+    console.log('查看数据')
+    console.log(dataMap)
 
-        async function openBrowser() {
-            console.log('正在启动 Chrome')
+    async function openBrowser() {
+      console.log('正在启动 Chrome')
 
-            const options = {
-                headless: true,
-                args: ["--window-position=0,0", `--window-size=1280,800`],
-                defaultViewport: {width: 1280, height: 800},
-                // devtools: true,
-                ignoreDefaultArgs: ["--enable-automation"]
-            }
-            const browser = await puppeteer.launch(options);
-            const [p] = await browser.pages()
-            return p // 递交page
-        }
-
-        async function login(p) {
-            if (process.env.EMAIL && process.env.PASSWORD) { // 输入账号密码
-                console.log('正在登录巨量')
-                const urls = [
-                    'https://business.oceanengine.com/login?appKey=51' // 巨量登录页
-                ]
-                const url = urls[0]
-                await p.goto(url);
-
-                console.log(process.env.EMAIL);
-
-                await sleep(3000); // 等待3s进入登录页
-
-                // 使用坐标点击最稳妥
-                const x = 950 // 纵坐标相同
-                await p.mouse.click(x, 345) // 账号
-                await p.keyboard.type(process.env.EMAIL, {delay: 80})
-                await p.mouse.click(x, 393) // 密码
-                await p.keyboard.type(process.env.PASSWORD, {delay: 80})
-
-                await p.mouse.click(820, 455) // 协议
-                await p.mouse.click(x, 500) // 登录
-            } else {
-                throw new Error('没有账号密码')
-            }
-        }
-
-        async function listen(p) {
-            // 开启响应监听
-            await p.on('response', async res => {
-                const fnMap = {
-                    getLiveRoomData, // 直播 -> 直播数据汇总
-                    getLivingData, // 直播 -> 正在直播
-                    getAnalysisLivingData // 分析 -> 直播分析 -> 直播间
-                };
-                Object.keys(dataMap).forEach(name => {
-                    fnMap[name](res).then(data => {
-                        if (data) dataMap[name] = data
-                    })
-                })
-            })
-        }
-
-        async function emit(p) {
-            await liveClick(p)
-            await analysisClick(p)
-        }
-
-    } catch (e) {
-        console.warn(e);
+      const options = {
+        headless: false,
+        args: ["--window-position=0,0", `--window-size=1280,800`],
+        defaultViewport: {width: 1280, height: 800},
+        // devtools: true,
+        ignoreDefaultArgs: ["--enable-automation"]
+      }
+      const browser = await puppeteer.launch(options);
+      const [p] = await browser.pages()
+      return p // 递交page
     }
+
+    async function login(p) {
+      if (process.env.EMAIL && process.env.PASSWORD) { // 输入账号密码
+        console.log('正在登录巨量')
+        console.log(process.env.EMAIL);
+
+        const urls = [
+          'https://business.oceanengine.com/login?appKey=51' // 巨量登录页
+        ]
+        const url = urls[0]
+        await p.goto(url);
+
+        await p.waitForSelector('.login-bg')
+
+        await sleep(1000);
+
+        // 使用坐标点击最稳妥
+        const x = 950 // 纵坐标相同
+        await p.mouse.click(x, 345) // 账号
+        await p.keyboard.type(process.env.EMAIL, {delay: 80})
+        await p.mouse.click(x, 393) // 密码
+        await p.keyboard.type(process.env.PASSWORD, {delay: 80})
+
+        await p.click('.check-box-container', {delay: 200}) // 协议
+
+        await p.click('.account-center-submit', {delay: 200}) // 登录
+        await sleep(3000)
+        await p.waitForSelector('.captcha_verify_container')
+
+        // 可能会出现验证码
+        const xy = await calculateDistance(p) //
+
+
+      } else {
+        throw new Error('没有账号密码')
+      }
+    }
+
+    async function listen(p) {
+      // 开启响应监听
+      await p.on('response', async res => {
+        const fnMap = {
+          getLiveRoomData, // 直播 -> 直播数据汇总
+          getLivingData, // 直播 -> 正在直播
+          getAnalysisLivingData // 分析 -> 直播分析 -> 直播间
+        };
+        Object.keys(dataMap).forEach(name => {
+          fnMap[name](res).then(data => {
+            if (data) dataMap[name] = data
+          })
+        })
+      })
+    }
+
+    async function emit(p) {
+      await liveClick(p)
+      await analysisClick(p)
+    }
+
+  } catch (e) {
+    console.warn(e);
+  }
 })();
--- js/common.js
+++ js/common.js
@@ -91,4 +91,47 @@ async function analysisClick(p) {
     await sleep(2000)
 }
 
-module.exports = {liveClick,analysisClick, getLivingData, getAnalysisLivingData, getLiveRoomData, sleep};
+
+/**
+ * 计算按钮需要滑动的距离
+ * */
+async function calculateDistance(page) {
+    const distance = await page.evaluate(() => {
+
+        // 比较像素,找到缺口的大概位置
+        function compare(document) {
+            console.log('document', document);
+
+            const ctx1 = document.querySelector('.captcha_verify_img_slide'); // 完成图片
+            const ctx2 = document.querySelector('.captcha-verify-image');  // 带缺口图片
+            const pixelDifference = 30; // 像素差
+            let res = []; // 保存像素差较大的x坐标
+
+            // 对比像素
+            for(let i=57;i<260;i++){
+                for(let j=1;j<160;j++) {
+                    const imgData1 = ctx1.getContext("2d").getImageData(1*i,1*j,1,1)
+                    const imgData2 = ctx2.getContext("2d").getImageData(1*i,1*j,1,1)
+                    const data1 = imgData1.data;
+                    const data2 = imgData2.data;
+                    const res1=Math.abs(data1[0]-data2[0]);
+                    const res2=Math.abs(data1[1]-data2[1]);
+                    const res3=Math.abs(data1[2]-data2[2]);
+                    if(!(res1 < pixelDifference && res2 < pixelDifference && res3 < pixelDifference)) {
+                        if(!res.includes(i)) {
+                            res.push(i);
+                        }
+                    }
+                }
+            }
+            // 返回像素差最大值跟最小值，经过调试最小值往左小7像素，最大值往左54像素
+            return {min:res[0]-7,max:res[res.length-1]-54}
+        }
+        return compare(document)
+    })
+    console.log('distance', distance);
+    return distance;
+}
+
+
+module.exports = {liveClick,analysisClick, getLivingData, getAnalysisLivingData, getLiveRoomData, sleep,calculateDistance};
